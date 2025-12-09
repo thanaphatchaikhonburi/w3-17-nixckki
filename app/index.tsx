74423de59f542d22dd45e7e8710a193a546e347e
thanaphatchaikhonburi <thanaphat.chaikhonburi@e-tech.ac.th>
2025-12-09 10:12:08 +0700

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,31 @@
+import { View, Text, Button, StyleSheet } from "react-native";
+import { useRouter } from "expo-router";
+
+
+
+export default function App() {
+
+const router = useRouter()
+
+
+    return (
+        <View style={styles.box}>
+            <Text style={styles.textTitle}>Home</Text>
+            <Button title=" ไปหน้า 2" onPress={() => router.navigate('/rectangle') }/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    box: {
+        flex: 1,
+        backgroundColor: "lightgray",
+        justifyContent: "center",
+        alignItems:"center"
+    },
+    textTitle: {
+        fontSize:22,
+        fontWeight:"700",
+        color:"white"
+    }
+})
---
+++ app/rectangle.tsx
@@ -0,0 +1,66 @@
+import { View, Text, Button, StyleSheet, TextInput} from "react-native";
+import { useState } from "react";
+export default function Rectangle() {
+//พื้นที่สี่เหลี่ยม =  กว้าง * ยาว 
+//       60   = 6 * 10
+// Output  = Input * Input 
+
+const [area , setArea] = useState(0) // area = 20 x, setArea(20) /
+const [width, setWidth] = useState(0)
+const [length, setLength] = useState(0)
+
+function rectangleCal(){
+var result = width * length
+setArea(result)
+}
+
+    return (
+        <View style={styles.box}>
+            <Text style={styles.textTitle}>คำนวณพื้นที่สี่เหลี่ยม</Text>
+ {/* <Button title=" ไปหน้า 2"/> */}
+         
+        <Text> ความกว้าง คือ {width} ซม. </Text>
+         <Text> ความยาว คือ {length} ซม. </Text>
+ <Text> พื้นที่สี่เหลี่ยม คือ {area} ตร.ซม. </Text>
+
+
+
+
+            <TextInput style={styles.TextInput} placeholder="กรอกความกว้าง" placeholderTextColor="gray"
+            value={width.toString()}
+            onChangeText={(w) => setWidth(w)}
+            />
+
+            <TextInput style={styles.TextInput} placeholder="กรอกความยาว" placeholderTextColor="gray"
+            
+            value={length.toString()}
+            onChangeText={(l) => setLength(Number(l))}
+            />
+      <Button title="คำนวณ" onPress={() => rectangleCal() } />
+      
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    box: {
+        flex: 1,
+        backgroundColor: "lightgray",
+        justifyContent: "center",
+        alignItems:"center",
+        gap: 20
+    },
+    textTitle: {
+        fontSize:22,
+        fontWeight:"700",
+        color:"white"
+    },
+    TextInput:{
+        borderWidth: 1,
+        borderColor: "white",
+        width: "80%",
+        borderRadius: 20
+    }
+
+})
+   
